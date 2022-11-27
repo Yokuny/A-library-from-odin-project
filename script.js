3aa5026a8c1263b4d539e9115ad194e938ca27f3
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-27 18:56:20 -0300

    i made more one
    in this project I made an element that displace online when you will register a new book. and after you have already registred this close and a function get all the content save all in a array of objects. and render the objects of this array in the screen

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-function run(){
-    function book(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+const renderRegisterInterface = document.getElementById("floatRegisterCard");
 
-    const clubDaLuta = new book('club da luta', 'David Finch', 300, true);
-    const aMeninaQueRoubavaLivros = new book('A menina que roubava livros', 'Markus Zusak', 240, true);
+function registerNewBook(){
+    renderRegisterInterface.style = "display:flex;"
+}
 
-    console.log(clubDaLuta);
+let myLibrary = [];
 
-    book.prototype.displayBookInfo = function(){
-        console.log(`Titulo: ${title}, Autor: ${author}, Paginas: ${pages}, Já foi lido?: ${read}`);
-    }
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
-    console.log(clubDaLuta);
+function register(){
+    let bookListElement = document.getElementById("bookList");
+    bookListElement.innerHTML = " ";
+    let bookTitleElement = document.getElementById("bookTitle").value;
+    let authorNameElement = document.getElementById("authorName").value;
+    let amountOfPagesElement = document.getElementById("amountOfPages").value;
+    let alreadReadElement = document.getElementById("readYes").checked;
+    let newBook = new Book(bookTitleElement, authorNameElement, amountOfPagesElement, alreadReadElement);
+    myLibrary.push(newBook);
+    myLibrary.forEach(book => {
+        let newLiElement = document.createElement("li");
+        let titleElement = document.createElement("h4");
+        titleElement.textContent = `${book.title}`;
+        let autorDisplaceElement = document.createElement("p");
+        autorDisplaceElement.textContent = " Autor:";
+        let autorElement = document.createElement("h4");
+        autorElement.textContent = `${book.author}`;
+        let pagesDisplaceElement = document.createElement("p");
+        pagesDisplaceElement.textContent = " paginas:";
+        let pageCountElement = document.createElement("h4");
+        pageCountElement.textContent = `${book.pages}`;
+
+        newLiElement.append(titleElement, autorDisplaceElement, autorElement, pagesDisplaceElement, pageCountElement);
+        if(book.read){
+            newLiElement.style.borderLeft = 'solid #23d5abd5 4px';
+        }else{
+            newLiElement.style.borderLeft = 'solid #d631ffce 4px';
+        }
+        bookListElement.appendChild(newLiElement);
+        console.log(newLiElement);
+    });
+
+
+    renderRegisterInterface.style = "display:none;"
 }
